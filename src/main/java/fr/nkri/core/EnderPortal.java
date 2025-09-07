f14e874e1d5977231dc829faf91517aa232f616d
NKRI <nkri.dev@gmail.com>
2025-09-07 20:40:56 +0200

    test : GET route /hello with API Key

--- src/main/java/fr/nkri/core/EnderPortal.java
+++ src/main/java/fr/nkri/core/EnderPortal.java
@@ -1,11 +1,16 @@
 package fr.nkri.core;
 
+import com.google.gson.Gson;
+import fr.nkri.core.api.APIClient;
 import fr.nkri.core.auth.AuthManager;
 import fr.nkri.core.auth.cmds.LoginCommand;
+import fr.nkri.core.users.User;
 import fr.nkri.core.users.UserManager;
 import fr.nkri.japi.JAPI;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
 
 public class EnderPortal extends JavaPlugin {
 
@@ -25,6 +30,19 @@ public class EnderPortal extends JavaPlugin {
 
         //register commands
         JAPI.getInstance().registerCommand(new LoginCommand());
+
+        /**
+         * TEST : CALL API -> /hello, protected route by API KEY
+         */
+        //TODO : changer l'API KEY, mettre dans un .yml qui ne sera pas push sur GITHUB
+        final String apiKey = "7E!HT$pmAT9o8kQfqfpJPFzPfA6mMpHrjYCSn&4P";
+
+        try {
+            final String response = APIClient.get("http://localhost:3000/hello", apiKey);
+            System.out.println(response);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
